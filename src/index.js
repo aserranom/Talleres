e08d67e352a180e6d9f7c27c024584a33ec96e24
Alvaro Serrano <marran51@gmail.com>
2019-03-28 00:24:03 -0300

    Our first component

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from "react-dom";
 
 import "./styles.css";
 
+function Subtitle() {
+  return <h2>Start editing to see some magic happen!</h2>;
+}
+
 function App({ name }) {
   return (
     <div className="App">
       <h1>Hello {name}</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <Subtitle />
     </div>
   );
 }
